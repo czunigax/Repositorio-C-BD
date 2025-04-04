7e73707a099d53a297764ab9d44224ead883c8e1
czunigax <bruno09cr82@gmail.com>
2025-04-03 22:55:05 -0600

    Update webapi.tf

--- webapi.tf
+++ webapi.tf
@@ -1,12 +1,10 @@
 resource "azurerm_app_service" "backend_api" {
   name                = "api-${var.name_Project}-${var.enviroment}"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rgproyecto.name  # Asegúrate que coincide con tu RG
-  app_service_plan_id = azurerm_service_plan.spproyecto.id      # Nombre corregido aquí
-  
+  resource_group_name = azurerm_resource_group.rgproyecto.name  
+  app_service_plan_id = azurerm_service_plan.spproyecto.id      
   app_settings = {
     "Redis__ConnectionString" = "tu_cadena_de_conexion_redis"
-    # Otras configuraciones...
   }
 
   site_config {
